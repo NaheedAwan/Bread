3181c1299622ddb27b721d88370c35aac601bcc2
Naheed Awan <nadsawan@yahoo.com>
2022-02-16 21:25:29 -0500

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -6,17 +6,17 @@ const Bread = require('../models/bread.js')
 // NEW
 
 breads.get('/', (req, res) => {
-    res.render('Index',
-      {
-        'breads': Bread
-      }
-    )
+  res.render('Index',
+    {
+      'breads': Bread
+    }
+  )
   // res.send(Bread)
 })
 // CREATE
 breads.post('/', (req, res) => {
   console.log(req.body)
-  if(req.body.hasGluten === 'on') {
+  if (req.body.hasGluten === 'on') {
     req.body.hasGluten === 'true'
   } else {
     req.body.hasGlutten === 'false'
@@ -24,26 +24,43 @@ breads.post('/', (req, res) => {
   Bread.push(req.body)
   res.redirect('/breads')
 })
-
-// NEW
-breads.get('/new', (req, res) => {
-  res.render('new')
-})
-  // show
-  breads.get('/:arrayIndex', (req,res)=>{
-    if (Bread[req.params.arrayIndex]){
-    res.render('Show', {
-      bread: Bread[req.params.arrayIndex],
-      index: req.params.arrayIndex,
-    })
-  } else{
-    res.send('404')
-  }
-  })
-  // DELETE
+// DELETE
 breads.delete('/:indexArray', (req, res) => {
   Bread.splice(req.params.indexArray, 1)
   res.status(303).redirect('/breads')
 })
+// UPDATE
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if (req.body.hasGluten === 'on') {
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
+})
+// edit
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
+
+
+// show
+breads.get('/:arrayIndex', (req, res) => {
+  if (Bread[req.params.arrayIndex]) {
+    res.render('Show', {
+      bread: Bread[req.params.arrayIndex],
+      index: req.params.arrayIndex,
+    })
+  } else {
+    res.send('404')
+  }
+})
+
 
 module.exports = breads
